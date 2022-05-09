c41dbc1297110fc8ec79414409767e195b4565ed
Baheya <khalifa.baheya@gmail.com>
2022-05-09 17:12:30 +0200

    updated select component with course solution code

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,27 +5,65 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
-const Select = ({ label, value, onChange, children }) => {
+const Select = ({ value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <Wrapper value={value} onChange={onChange} width={displayedValue.length}>
-      {children}
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <PresentationalBit>
+        {displayedValue}
+        <IconWrapper style={{ '--size': 24 + 'px' }}>
+          <Icon id="chevron-down" strokeWidth={1} size={24} />
+        </IconWrapper>
+      </PresentationalBit>
     </Wrapper>
   );
 };
 
-const Wrapper = styled.select`
-  width: ${(props) => props.width + 6 + 'ch'};
-  background-color: ${COLORS.transparentGray15};
-  border: none;
-  color: ${COLORS.gray700};
-  border-radius: 8px;
-  padding: 12px 16px;
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
 
-  &:hover {
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  /* Allow the select to span the full height in Safari */
+  -webkit-appearance: none;
+`;
+
+const PresentationalBit = styled.div`
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.transparentGray15};
+  font-size: ${16 / 16}rem;
+  padding: 12px 16px;
+  padding-right: 52px;
+  border-radius: 8px;
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+  ${NativeSelect}:hover + & {
     color: ${COLORS.black};
   }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 10px;
+  margin: auto;
+  width: var(--size);
+  height: var(--size);
+  pointer-events: none;
 `;
 
 export default Select;
