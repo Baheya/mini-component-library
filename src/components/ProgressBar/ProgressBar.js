43bb3c570b03d4a9d90ca02a27fdeaf3b45ee9f1
Baheya <khalifa.baheya@gmail.com>
2022-05-09 16:47:12 +0200

    updated progress bar with code from solution video

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,57 +5,73 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const SIZES = {
+const STYLES = {
   small: {
-    '--height': 8 + 'px',
-    '--padding': 0,
-    '--borderRadius': 4 + 'px',
+    height: 8,
+    padding: 0,
+    radius: 4,
   },
   medium: {
-    '--height': 12 + 'px',
-    '--padding': 0,
-    '--borderRadius': 4 + 'px',
+    height: 12,
+    padding: 0,
+    radius: 4,
   },
   large: {
-    '--height': 24 + 'px',
-    '--padding': 4 + 'px',
-    '--borderRadius': 8 + 'px',
+    height: 16,
+    padding: 4,
+    radius: 8,
   },
 };
 
 const ProgressBar = ({ label, value, size }) => {
-  const style = SIZES[size];
+  const styles = STYLES[size];
+
+  if (!styles) {
+    throw new Error(`Unknown size passed to ProgressBar: ${size}`);
+  }
+
   return (
-    <>
-      <VisuallyHidden as="label" for="loadinglabel">
-        {label}
-      </VisuallyHidden>
-      <Wrapper
-        role="progressbar"
-        aria-labelledby="loadinglabel"
-        aria-valuenow={value}
-        style={style}
-      >
-        <Bar width="100%">
-          <rect height="100%" width={`${value}%`} fill={COLORS.primary} />
-        </Bar>
-      </Wrapper>
-    </>
+    <Wrapper
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+      style={{
+        '--padding': styles.padding + 'px',
+        '--radius': styles.radius + 'px',
+      }}
+    >
+      <VisuallyHidden>{value}%</VisuallyHidden>
+      <BarWrapper>
+        <Bar
+          style={{
+            '--width': value + '%',
+            '--height': styles.height + 'px',
+          }}
+        />
+      </BarWrapper>
+    </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
-  background-color: ${COLORS.gray50};
+  background-color: ${COLORS.transparentGray15};
   box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
-  border-radius: 8px;
-  display: flex;
-  align-items: center;
+  border-radius: var(--radius);
   padding: var(--padding);
 `;
 
-const Bar = styled.svg`
+const BarWrapper = styled.div`
   border-radius: 4px;
+  /* Trim off corners when progress bar is near-full. */
+  overflow: hidden;
+`;
+
+const Bar = styled.div`
+  width: var(--width);
   height: var(--height);
+  background-color: ${COLORS.primary};
+  border-radius: 4px 0 0 4px;
 `;
 
 export default ProgressBar;
